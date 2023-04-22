1bfeaeaacc3ca9dd3ae122da988462da97bbc524
rafaellopes74 <91756990+rafaellopes74@users.noreply.github.com>
2023-04-22 13:21:29 +0200

    first commit

---
+++ src/main/java/com/ManumlandAPI/ManunlandAPI/ManunlandApiApplication.java
@@ -0,0 +1,13 @@
+package com.ManumlandAPI.ManunlandAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManunlandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManunlandApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ManumlandAPI/ManunlandAPI/Model/UserRole.java
@@ -0,0 +1,7 @@
+package com.ManumlandAPI.ManunlandAPI.Model;
+
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
